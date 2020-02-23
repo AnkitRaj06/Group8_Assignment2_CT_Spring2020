5f1f91c35ac38ddf1f63988642cbf1891a53bae1
Ankit Raj <ankitraj@usf.edu>
2020-02-23 17:46:31 -0500

    Comments added in 4, 5 & 6

--- Program.cs
+++ Program.cs
@@ -154,10 +154,16 @@ namespace Group8_Assignment2_CT_Spring2020
         }
         public static string FreqSort(string s2)
         {
+            /* Logic: Created a dictionary to store the characters and their counts in the string, 
+             sorted the dictionary and then stored the sorted keys from the dictionary depending 
+             on their number of occurances in a new string.*/
+
+            //String to store the output
             String freqSorted = "";
             try
             {
                 //Write Your Code Here
+                //Dictionary to store the string elements and their counts
                 Dictionary<char, int> dictionary = new Dictionary<char, int>();
                 foreach(char c in s2)
                 {
@@ -170,9 +176,13 @@ namespace Group8_Assignment2_CT_Spring2020
                         dictionary.Add(c, 1);
                     }
                 }
+                //Sorted the dictionary in descending order
                 dictionary = dictionary.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+                //Iterated over each key-value of the sorted list
                 foreach(KeyValuePair<char, int> item in dictionary)
                 {
+                    //Iterated to ensure each key is stored multiple times as per their values
                     for(int i=0; i<item.Value; i++)
                     {
                         freqSorted += item.Key;
@@ -187,6 +197,11 @@ namespace Group8_Assignment2_CT_Spring2020
         }
         public static int[] Intersect1(int[] nums1, int[] nums2)
         {
+            /*Logic: Used a while loop to iterate over each item of the two sorted arrays, store them in a string 
+             if there is a value match, and increase their indices, else keep the larger array value fixed, and go
+             to the next value of the array having a smaller value. */
+
+            //Integer array to store the final return output
             int[] intersect = new int[] { };
             try
             {
@@ -194,14 +209,18 @@ namespace Group8_Assignment2_CT_Spring2020
                 int len1 = nums1.Length;
                 int len2 = nums2.Length;
 
+                //A string variable to store the matching values
                 string intersectedValues = "";
 
+                //Sorted both the arrays
                 Array.Sort(nums1);
                 Array.Sort(nums2);
 
+                //Variables used to iterate over the indices of the two sorted arrays
                 int i = 0;
                 int j = 0;
 
+                //In case any of the arrays is null, returned an ampty integer array
                 if(nums2 is null || nums2 is null)
                 {
                     intersect = new int[] { };
@@ -209,14 +228,17 @@ namespace Group8_Assignment2_CT_Spring2020
 
                 else
                 {
+                    //While loop to iterate over the two sorted arrays
                     while (i < len1 && j < len2)
                     {
+                        //If there is a value match, value stored in a string and both i, j are increased
                         if (nums1[i] == nums2[j])
                         {
                             intersectedValues += nums1[i];
                             i++;
                             j++;
                         }
+                        //i/j are increased to compare next value of array with a smaller value
                         else if (nums1[i] < nums2[j])
                         {
                             i++;
@@ -227,8 +249,10 @@ namespace Group8_Assignment2_CT_Spring2020
                         }
                     }
                 }
-                
+                //Length of the interger array is instantiated as the length of the string
                 intersect = new int[intersectedValues.Length];
+
+                //For loop to store the string values to the integer array
                 for (int k = 0; k < intersectedValues.Length; k++)
                 {
                     intersect[k] = int.Parse(intersectedValues[k].ToString());
@@ -242,10 +266,16 @@ namespace Group8_Assignment2_CT_Spring2020
         }
         public static int[] Intersect2(int[] nums1, int[] nums2)
         {
+            /*Logic: Used a dictionary to store each nums1 element and their counts, then iterate over 
+            each element of nums2 and if the element exists as a key in the dictionary, then add it in 
+            a string and reduce the count of that key by 1, else remove that key from the dictionary */
+
+            //Integer array to store the final return output
             int[] intersect = new int[] { };
             try
             {
                 // Write your code here
+                //Dictionary to store the nums1 elements and their counts
                 Dictionary<int, int> dict = new Dictionary<int, int>();
                 foreach(int i in nums1)
                 {
@@ -258,7 +288,11 @@ namespace Group8_Assignment2_CT_Spring2020
                         dict.Add(i, 1);
                     }
                 }
+
+                //String to store the intersecting elements
                 string intersectedValues = "";
+
+                //For loop to iterate over each element of nums2 to find an intersect
                 foreach (int i in nums2)
                 {
                     if (dict.ContainsKey(i))
@@ -274,7 +308,10 @@ namespace Group8_Assignment2_CT_Spring2020
                         }
                     }
                 }
+                // Length of the integer array is set as the length of the string
                 intersect = new int[intersectedValues.Length];
+
+                //For loop to store the string values to the integer array
                 for(int i=0; i < intersectedValues.Length; i++)
                 {
                     intersect[i] = int.Parse(intersectedValues[i].ToString());
@@ -289,27 +326,36 @@ namespace Group8_Assignment2_CT_Spring2020
 
         public static bool ContainsDuplicate(char[] arr, int k)
         {
+            /* Logic: A dictionary is used to store the char element by iterating over a for loop, 
+             and in case it exists in the dictionary, its value is stored in a new variable index 
+             and the difference between both the indices is checked against k */ 
             try
             {
                 //Write your code here;
+                //Dictionary to store a new char value
                 Dictionary<char, int> dict = new Dictionary<char, int>();
+
+                //For loop to iterate over each char element
                 for(int i = 0; i < arr.Length; i++)
                 {
-                    int index1 = 0;
-                    //int index2 = 0;
+                    //Variables to store the old index value and the difference
+                    int index = 0;
                     int diff = 0;
+
+                    //If the dictionary contains the char element, the difference between their index values is calculated
                     if (dict.ContainsKey(arr[i]))
                     {
-                        index1 = dict[arr[i]];
-                        diff = i - index1;
+                        index = dict[arr[i]];
+                        diff = i - index;
                         dict.Remove(arr[i]);
                         dict.Add(arr[i], i);
                     }
                     else
                     {
-                        //index1 = i;
                         dict.Add(arr[i], i);
                     }
+
+                    //If the difference is > 0, but <= k, our condition is satisfied
                     if(diff > 0 && diff <= k)
                     {
                         return true;
